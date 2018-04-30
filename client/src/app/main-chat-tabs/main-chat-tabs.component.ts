8a01c7ad1bc823653edbd1aa50610e40f827658d
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-04-30 14:54:56 -0700

    Add (correct) resize events

--- client/src/app/main-chat-tabs/main-chat-tabs.component.ts
+++ client/src/app/main-chat-tabs/main-chat-tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit, ViewChild, Output, EventEmitter, Input } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild, Output, EventEmitter, Input, ElementRef } from '@angular/core';
 import { ChatTab } from "./main-chat-tabs.types";
 
 @Component({
@@ -25,14 +25,20 @@ export class MainChatTabsComponent implements OnInit {
 	public tabs: ChatTab[] = [];
 	public tabWidth: number = 180;
 
-	@ViewChild("container") private containerRef;
-	public get container() { return this.containerRef.nativeElement; }
+	@ViewChild("container") private containerRef: ElementRef;
+	public get container(): HTMLElement { return this.containerRef.nativeElement; }
 
 	ngOnInit() {
 		this.addTab("Dylan Bienenstock");
 		this.addTab("Dylan Bienenstock");
 		this.addTab("Dylan Bienenstock");
 		this.addTab("Dylan");
+
+		this.listenForResize();
+	}
+
+	ngAfterViewInit() {
+		this.listenForResize();
 	}
 
 	addTab(title: string) {
@@ -48,12 +54,45 @@ export class MainChatTabsComponent implements OnInit {
 		}
 
 		this.tabs.push(tab);
+
+		setTimeout(() => {
+			this.resizeTabs();
+		}, 0);
 	}
 
+	public resizing: boolean = false;
 	public selectedTab: ChatTab;
 	private dragging: boolean = false;
 	private draggingTab: ChatTab;
 	private dragStartX: number;
+
+	resizeTabs() {
+		this.resizing = true;
+
+		let tabWidth = this.container.clientWidth / this.tabs.length;
+		this.tabWidth = Math.min(tabWidth, 180);
+
+		this.fixTabPositions();
+
+		setTimeout(() => {
+			this.resizing = false;
+		});
+	}
+
+	listenForResize() {
+		if (this.sidebars.contacts.animating ||
+			this.sidebars.options.animating) {
+			
+			this.resizeTabs();
+		}
+
+		requestAnimationFrame(this.listenForResize.bind(this));
+	}
+
+	@HostListener("window:resize", ["$event"])
+	onWindowResize(e) {
+		this.resizeTabs();
+	}
 
 	onMouseDown(tab: ChatTab, e: MouseEvent) {
 		this.dragging = true;
@@ -89,43 +128,47 @@ export class MainChatTabsComponent implements OnInit {
 			let containerWidth = this.container.clientWidth - this.tabWidth + 3;
 			this.draggingTab.offsetX = Math.min(containerWidth, Math.max(0, difference));
 
-			// Copy the array
-			let tabs = Object.assign([], this.tabs);
+			this.fixTabPositions();
+		}
+	}
 
-			// Sort the new array by order
-			tabs.sort((a: ChatTab, b:ChatTab) => {
-				return a.order - b.order;
-			});
+	fixTabPositions() {
+		// Copy the array
+		let tabs = Object.assign([], this.tabs);
 
-			// Sort by position
-			tabs.sort((a: ChatTab, b: ChatTab) => {
-				let aLeft = a.offsetX;
-				let aWidth = this.tabWidth;
-				let aMiddle = aLeft + aWidth / 2;
-				let aRight = aLeft + aWidth;
+		// Sort the new array by order
+		tabs.sort((a: ChatTab, b: ChatTab) => {
+			return a.order - b.order;
+		});
 
-				let bLeft = b.offsetX;
-				let bWidth = this.tabWidth;
-				let bMiddle = bLeft + bWidth / 2;
-				
-				if (aMiddle > bLeft) return 1;
-				if (aRight < bMiddle) return -1;
+		// Sort by position
+		tabs.sort((a: ChatTab, b: ChatTab) => {
+			let aLeft = a.offsetX;
+			let aWidth = this.tabWidth;
+			let aMiddle = aLeft + aWidth / 2;
+			let aRight = aLeft + aWidth;
 
-				return 0;
-			});
+			let bLeft = b.offsetX;
+			let bWidth = this.tabWidth;
+			let bMiddle = bLeft + bWidth / 2;
 
-			// Copy the indices of sorted array into tab.order
-			this.tabs.forEach((tab) => {
-				tab.order = tabs.indexOf(tab);
-			});
+			if (aMiddle > bLeft) return 1;
+			if (aRight < bMiddle) return -1;
 
-			// Set their positions based on their order
-			for (let i = 0; i < this.tabs.length; i++) {
-				let tab = this.tabs.find(tab => tab.order == i);
-				
-				if (tab != this.draggingTab) {
-					tab.offsetX = tab.order * this.tabWidth;
-				}
+			return 0;
+		});
+
+		// Copy the indices of sorted array into tab.order
+		this.tabs.forEach((tab) => {
+			tab.order = tabs.indexOf(tab);
+		});
+
+		// Set their positions based on their order
+		for (let i = 0; i < this.tabs.length; i++) {
+			let tab = this.tabs.find(tab => tab.order == i);
+
+			if (tab != this.draggingTab) {
+				tab.offsetX = tab.order * this.tabWidth;
 			}
 		}
 	}
