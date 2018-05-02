85b0c3615fa6e9c94f2f98a87035b2327886a4a9
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-01 21:46:35 -0700

    Add additional validators to name and email

--- gamma/account/schemas.ts
+++ gamma/account/schemas.ts
@@ -1,19 +1,20 @@
 import * as mongoose from "mongoose"
 
+const nameRegex: RegExp = /^([a-zA-Z0-9\-_])*$/;
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const User = mongoose.model("User", {
 	name: {
 		type: String,
 		required: [true, "Username is required"],
+		match: [nameRegex, "Username must contain only letters, numbers, and hyphens"],
 		maxlength: [24, "Username must be fewer than 24 characters"]
 	},
 
-	// Required for account recovery
 	email: {
 		type: String,
-		required: false,
-		match: [emailRegex, "Email format is invalid"]
+		required: [true, "Email address is required"],
+		match: [emailRegex, "Email address is invalid"]
 	},
 
 	// Hashed using bcrypt
