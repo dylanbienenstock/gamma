a9cc441fb7c808a72375092ada7619b3603a0ba5
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-02 17:35:06 -0700

    Make searchbar search

--- client/src/app/main-contacts-search/main-contacts-search.component.ts
+++ client/src/app/main-contacts-search/main-contacts-search.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { SearchQuery } from '../../../../gamma/account/types';
+import { SocketService } from '../socket.service';
 
 @Component({
-  selector: 'app-main-contacts-search',
-  templateUrl: './main-contacts-search.component.html',
-  styleUrls: ['./main-contacts-search.component.scss']
+	selector: 'app-main-contacts-search',
+	templateUrl: './main-contacts-search.component.html',
+	styleUrls: ['./main-contacts-search.component.scss']
 })
 export class MainContactsSearchComponent implements OnInit {
 
-  constructor() { }
+	constructor(private socketService: SocketService) { }
 
-  ngOnInit() {
-  }
+	@Input() localUser: any;
 
+	queryText: string;
+	searchTimeout: any;
+	searchSubscription: Subscription;
+
+	ngOnInit() {
+
+	}
+
+	onNgModelChange() {
+		clearTimeout(this.searchTimeout);
+
+		this.searchTimeout = setTimeout(() => {
+			this.search({
+				auth: this.localUser.authCreds(),
+				text: this.queryText
+			});
+		}, 250);
+	}
+
+	search(query: SearchQuery) {
+		console.log(query);
+
+		if (this.searchSubscription) {
+			this.searchSubscription.unsubscribe();
+		}
+
+		let observable = this.socketService.search(query);
+		this.searchSubscription = observable.subscribe((data) => {
+			console.log(data);
+			this.searchSubscription.unsubscribe();
+		});
+	}
 }
