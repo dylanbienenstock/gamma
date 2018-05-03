d2a448ee6b89de62d5f1ced39475a6c2af67ba6b
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-03 01:18:52 -0700

    Add main-contacts-user component

--- client/src/app/main-contacts-user/main-contacts-user.component.ts
+++ client/src/app/main-contacts-user/main-contacts-user.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { SearchResult } from '../../../../gamma/account/types';
 
 @Component({
-  selector: 'app-main-contacts-user',
-  templateUrl: './main-contacts-user.component.html',
-  styleUrls: ['./main-contacts-user.component.scss']
+	selector: 'app-main-contacts-user',
+	templateUrl: './main-contacts-user.component.html',
+	styleUrls: ['./main-contacts-user.component.scss']
 })
-export class MainContactsUserComponent implements OnInit {
+export class MainContactsUserComponent implements AfterViewInit {
+	constructor() { }
 
-  constructor() { }
+	@Input() data: SearchResult;
+	@Input() index: number;
 
-  ngOnInit() {
-  }
+	hidden: boolean = true;
+	animationDelay: number = 75;
 
+	ngAfterViewInit() {
+		setTimeout(() => {
+			this.hidden = false;
+		}, (this.index + 1) * this.animationDelay)
+	}
 }
