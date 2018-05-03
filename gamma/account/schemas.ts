5610b90b3c65633c40df07d27c687f6a15a02a18
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-02 17:34:33 -0700

    Add friend schemas

--- gamma/account/schemas.ts
+++ gamma/account/schemas.ts
@@ -1,9 +1,38 @@
 import * as mongoose from "mongoose"
 
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
 const nameRegex: RegExp = /^([a-zA-Z0-9\-_])*$/;
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const User = mongoose.model("User", {
+const friendInviteSchema = new Schema({
+	friendId: ObjectId,
+	confirmed: {
+		type: Boolean,
+		default: false
+	}
+},
+{
+	timestamps: true
+});
+
+const friendSchema = new Schema({
+	friendId: ObjectId,
+	confirmed: {
+		type: Boolean,
+		default: false
+	},
+	lastMessage: {
+		type: Date,
+		default: Date.now
+	}
+},
+{
+	timestamps: true
+});
+
+const userSchema = new Schema({
 	name: {
 		type: String,
 		required: [true, "Username is required"],
@@ -23,7 +52,17 @@ const User = mongoose.model("User", {
 		required: [true, "Password is required"]
 	},
 
-	authToken: String
+	// Random 16 byte string
+	authToken: String,
+
+	friendInvites: [friendInviteSchema],
+	friends: [friendSchema]
+},
+{
+	timestamps: true
 });
+
+
+const User = mongoose.model("User", userSchema);
 
 export { User }
