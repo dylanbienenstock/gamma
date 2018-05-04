912ad758618c7f5002d251ccf92bd85989ca4df8
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-04 01:39:06 -0700

    Add "you" and "others" to main-contacts-list

--- client/src/app/main-contacts-list/main-contacts-list.component.ts
+++ client/src/app/main-contacts-list/main-contacts-list.component.ts
@@ -11,19 +11,27 @@ export class MainContactsListComponent {
 
 	constructor() { }
 
+	you: Contact[]; // It's only an array because I'm lazy.
 	requests: Contact[]; // Received invitations
 	invitations: Contact[]; // Send invitatons
 	friends: Contact[]; // Confirmed friends
+	others: Contact[]; // Confirmed friends
 
 	@Input() localUser: any;
 
 	@Input() set contacts(value: ContactList) {
+		this.you = value.contacts.filter(value =>
+			value.isSelf &&
+			!value.isFriend &&
+			!value.isConfirmed &&
+			!value.isRequesting
+		) || [];
+
 		this.requests = value.contacts.filter(value =>
 			!value.isSelf &&
 			!value.isFriend &&
 			!value.isConfirmed &&
 			value.isRequesting
-		);
 		) || [];
 
 		this.invitations = value.contacts.filter(value =>
@@ -31,7 +39,6 @@ export class MainContactsListComponent {
 			value.isFriend &&
 			!value.isConfirmed &&
 			!value.isRequesting
-		);
 		) || [];
 
 		this.friends = value.contacts.filter(value =>
@@ -40,6 +47,27 @@ export class MainContactsListComponent {
 			value.isConfirmed &&
 			!value.isRequesting
 		) || [];
+
+		this.others = value.contacts.filter(value =>
+			!value.isSelf &&
+			!value.isFriend &&
+			!value.isConfirmed &&
+			!value.isRequesting
+		) || [];
 	}
+
+	onAccepted(index: number) {
+		// $user-container-animation-duration in
+		// main-contacts-user.component.scss
+		let animationDuration: number = 250;
+
+		setTimeout(() => {
+			this.requests[index].isFriend = true;
+			this.requests[index].isConfirmed = true;
+			this.requests[index].isRequesting = false;
+
+			this.friends.unshift(this.requests[index]);
+			this.requests.splice(index);
+		}, animationDuration);
 	}
 }
