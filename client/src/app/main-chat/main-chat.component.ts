07b8a783b6854ddfa77764432ad45a16f8075d52
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-08 04:02:34 -0700

    Make main-chat store current conversation

--- client/src/app/main-chat/main-chat.component.ts
+++ client/src/app/main-chat/main-chat.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { SocketService } from '../socket.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Conversation } from '../chat.service.types';
+import { ChatService } from '../chat.service';
 
 @Component({
 	selector: 'app-main-chat',
 	templateUrl: './main-chat.component.html',
 	styleUrls: ['./main-chat.component.scss']
 })
-export class MainChatComponent  {
-	constructor() { }
+export class MainChatComponent {
+	constructor(private chatService: ChatService) { }
 
 	@Input() sidebars;
 	@Output() toggleSidebar: EventEmitter<string> = new EventEmitter<string>();
+
+	currentConversation: Conversation;
 
 	onToggleContacts() {
 		this.toggleSidebar.emit("contacts");
@@ -17,5 +23,9 @@ export class MainChatComponent  {
 
 	onToggleOptions() {
 		this.toggleSidebar.emit("options");
+	}
+
+	onTabClicked(conversation: Conversation) {
+		this.currentConversation = conversation;
 	}
 }
