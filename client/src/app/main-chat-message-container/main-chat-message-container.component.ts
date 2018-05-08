b0b83d5e72f59c0a98a324dfc815129b197d062f
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-08 04:01:14 -0700

    Make message container accept conversation as input

--- client/src/app/main-chat-message-container/main-chat-message-container.component.ts
+++ client/src/app/main-chat-message-container/main-chat-message-container.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Conversation } from '../chat.service.types';
+import { ChatService } from '../chat.service';
+import { Message } from '../../../../server/server.types';
+import { ContactService } from '../contact.service';
 
 @Component({
-  selector: 'app-main-chat-message-container',
-  templateUrl: './main-chat-message-container.component.html',
-  styleUrls: ['./main-chat-message-container.component.scss']
+	selector: 'app-main-chat-message-container',
+	templateUrl: './main-chat-message-container.component.html',
+	styleUrls: ['./main-chat-message-container.component.scss']
 })
 
-export class MainChatMessageContainerComponent implements OnInit {
+export class MainChatMessageContainerComponent {
 
-  constructor() { }
+	constructor(private chatService: ChatService,
+				private contactService: ContactService) { }
 
-  ngOnInit() {
-  }
-
+	@Input() conversation: Conversation;
 }
