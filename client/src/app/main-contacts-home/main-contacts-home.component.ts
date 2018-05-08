252202fe2f5f65e550a5216a6abca2c9a9333a35
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-08 03:57:18 -0700

    Save friend names using saveFriendNames

--- client/src/app/main-contacts-home/main-contacts-home.component.ts
+++ client/src/app/main-contacts-home/main-contacts-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ContactList } from '../../../../gamma/account/account.types';
 import { SocketService } from '../socket.service';
 import { LocalUserService } from '../local-user.service';
+import { ContactService } from '../contact.service';
 
 @Component({
 	selector: 'app-main-contacts-home',
@@ -11,7 +12,8 @@ import { LocalUserService } from '../local-user.service';
 export class MainContactsHomeComponent implements OnInit {
 
 	constructor(private socketService: SocketService,
-				private localUserService: LocalUserService) { }
+				private localUserService: LocalUserService,
+				private contactService: ContactService) { }
 
 	contactList: ContactList = {
 		contacts: []
@@ -26,6 +28,7 @@ export class MainContactsHomeComponent implements OnInit {
 			subscription.unsubscribe();
 
 			this.contactList = contactList;
+			this.contactService.saveFriendNames(contactList);
 		});
 	}
 }
