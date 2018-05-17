86d81e17e0bfb53a78f7dc888700d7f7ef67f91a
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-05-17 03:41:00 -0700

    Implement autoscroll

--- client/src/app/main-chat/main-chat.component.ts
+++ client/src/app/main-chat/main-chat.component.ts
@@ -1,8 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { SocketService } from '../socket.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Conversation } from '../chat.service.types';
 import { ChatService } from '../chat.service';
+import { ScrollbarComponent } from 'ngx-scrollbar';
+import { Message } from '../../../../gamma/gamma.types';
+import { LocalUserService } from '../local-user.service';
 
 @Component({
 	selector: 'app-main-chat',
@@ -10,13 +13,40 @@ import { ChatService } from '../chat.service';
 	styleUrls: ['./main-chat.component.scss']
 })
 export class MainChatComponent {
-	constructor(private chatService: ChatService) { }
+	constructor(private chatService: ChatService) {
+		this.onMessageSubscription =
+		this.chatService.onMessage
+		.subscribe((message: Message) => {
+			if (!this.autoScroll ||
+				!this.scrollbarRef ||
+				!this.scrollbarRef.view) return;
+			
+			let messageInCurrentConvo = 
+				message.recipientId == this.currentConversation.withId ||
+				message.senderId == this.currentConversation.withId;
+
+			if (messageInCurrentConvo) {
+				setTimeout(() => {
+					let scrollMax: number = this.scrollbarRef.view.scrollHeight;
+					
+					this.scrollbarRef.view.scrollTop = scrollMax;
+					this.autoScrollInProgress = true;
+				}, 0);
+			}
+		});
+	}
 
 	@Input() sidebars;
 	@Output() toggleSidebar: EventEmitter<string> = new EventEmitter<string>();
 	@Output() hideSidebars: EventEmitter<null> = new EventEmitter<null>();
 
+	@ViewChild("scrollbar") scrollbarRef: ScrollbarComponent;
+
+	onMessageSubscription: Subscription;
 	currentConversation: Conversation;
+	autoScroll: boolean = true;
+	autoScrollInProgress: boolean = false;
+	autoScrollTimeout: any;
 
 	onToggleContacts() {
 		this.toggleSidebar.emit("contacts");
@@ -32,5 +62,21 @@ export class MainChatComponent {
 
 	onHideSidebars() {
 		this.hideSidebars.emit(null);
+	}
+
+	onScrollState(e) {
+		if (!this.scrollbarRef.view) return;
+
+		if (this.autoScrollInProgress) {
+			this.autoScrollInProgress = false;
+			
+			return;
+		}
+
+		let distanceToBottom = this.scrollbarRef.view.scrollHeight;
+		distanceToBottom -= this.scrollbarRef.view.scrollTop;
+		distanceToBottom -= this.scrollbarRef.view.clientHeight;
+
+		this.autoScroll = (distanceToBottom <= 128);
 	}
 }
